21449c9d916ed90069bd0ac8adbf0caeda4953fd
olabao <051710a@gmail.com>
2025-06-01 01:47:16 +0800

    Finish layout of task page

--- app/Tasks.js
+++ app/Tasks.js
@@ -1,85 +1,92 @@
 import React, { useState, useEffect } from "react"
 import { ScrollView, View, Text, Pressable } from "react-native"
-import { fetchUserTask } from "@/firebaseAPI";
 import styles from "./Tasks.styles"
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Platform } from 'react-native';
 
 const backend_url =
     Platform.OS === 'web'
-        ? 'http://localhost:3000'       // For browser or Expo web
-        : 'http://192.168.1.15:3000';   // For physical phone
+        ? 'http://192.168.199.81:3000'       // For browser or Expo web
+        : 'http://192.168.199.81:3000';   // For physical phone
 
 const Tasks = ({ navigation }) => {
-    const [Tasks, setTasks] = useState([])
-    const [SortedBy, setSortedBy] = useState("deadline") // "deadline" or "expectedTime"
+    const [rootTasks, setRootTasks] = useState([])
+    const [leafTasks, setLeafTasks] = useState([])
+    const [sortedBy, setSortedBy] = useState("deadline")
+    const [showCompleted, setShowCompleted] = useState(false)
+    const [showRootTasks, setShowRootTasks] = useState(true)
 
     const getRootTasks = async () => {
-        // try {
-        //     const userID = "MI51nEam3GgPqbV7WQeP";
-
-        //     const [TasksData, MeetingData] = await Promise.all([
-        //         fetchUserTask(userID),
-        //     ]);
-        //     setTasks(TasksData);
-
-        //     console.log("Data fetched successfully", TasksData);
-        // } catch (error) {
-        //     console.error("Failed to fetch data: ", error);
-        // }
         const userID = "MI51nEam3GgPqbV7WQeP";
-        console.log("Fetching tasks for user:", userID);
         const res = await fetch(`${backend_url}/users/${userID}/tasks/root`);
-        setTasks(await res.json());
+        setRootTasks((await res.json()).tasks);
+    }
+
+    const getLeafTasks = async () => {
+        const userID = "MI51nEam3GgPqbV7WQeP";
+        const res = await fetch(`${backend_url}/users/${userID}/tasks/leaf`);
+        setLeafTasks((await res.json()).tasks);
     }
 
     useEffect(() => {
         getRootTasks()
+        getLeafTasks()
     }, [])
 
     return (
         <View style={{ flex: 1, backgroundColor: "#F0EFF6" }}>
             <ScrollView contentContainerStyle={styles.tasks}>
                 <View style={styles.panel}>
-                    <Pressable style={styles.button} onPress={() => setSortedBy("deadline")}>
-                        <Text style={styles.buttonText}>依截止日期</Text>
+                    <Pressable style={styles.button} onPress={() => sortedBy === "deadline" ? setSortedBy("expectedTime") : setSortedBy("deadline")}>
+                        {sortedBy === "deadline" ? <Text style={styles.buttonText}><Icon name="sort" /> 依截止日期</Text> : <Text style={styles.buttonText}><Icon name="sort" /> 依所需時間</Text>}
                     </Pressable>
-                    <Pressable style={styles.button} onPress={() => setSortedBy("expectedTime")}>
-                        <Text style={styles.buttonText}>依所需時間</Text>
+                    <Pressable style={styles.button} onPress={() => setShowCompleted(!showCompleted)}>
+                        {showCompleted === true ? <Text style={styles.buttonText}>已完成</Text> : <Text style={styles.buttonText}>未完成</Text>}
                     </Pressable>
-                    <Pressable style={styles.button} onPress={() => getRootTasks()}>
-                        <Text style={styles.buttonText}>切換已完成</Text>
+                    <Pressable style={styles.button} onPress={() => setShowRootTasks(!showRootTasks)}>
+                        {showRootTasks === true ? <Text style={styles.buttonText}>主要任務</Text> : <Text style={styles.buttonText}>任務細項</Text>}
                     </Pressable>
                 </View>
                 <View style={styles.tasksTaskList}>
                     {
-                        Tasks.length > 0 ? (
-                            Tasks.sort((a, b) => {
-                                if (SortedBy === "deadline") {
-                                    return new Date(a.EndTime) - new Date(b.EndTime);
-                                } else if (SortedBy === "expectedTime") {
-                                    return a.ExpectedTime - b.ExpectedTime;
+                        (() => {
+                            const currentTasks = showRootTasks ? rootTasks : leafTasks;
+                            const filteredTasks = currentTasks.filter((task) => {
+                                if (!showCompleted) {
+                                    return task.UnfinishedMember.includes("MI51nEam3GgPqbV7WQeP");
+                                } else {
+                                    return !task.UnfinishedMember.includes("MI51nEam3GgPqbV7WQeP");
                                 }
-                                return 0;
-                            }).map((task, index) => (
-                                <View key={index} style={styles.tasksTaskPreview}>
-                                    <View>
-                                        <Text style={styles.tasksTaskPreviewTitle}>{task.TaskName}</Text>
-                                        <Text style={styles.tasksDate}>{task.EndTime ? new Date(task.EndTime).toLocaleString("zh-TW", {
-                                            year: 'numeric',
-                                            month: '2-digit',
-                                            day: '2-digit',
-                                            hour: '2-digit',
-                                            minute: '2-digit',
-                                            hour12: true
-                                        }).replace(/\//g, "/") : "無"}</Text>
+                            });
+
+                            return filteredTasks.length > 0 ? (
+                                filteredTasks.sort((a, b) => {
+                                    if (sortedBy === "deadline") {
+                                        return new Date(a.EndTime) - new Date(b.EndTime);
+                                    } else if (sortedBy === "expectedTime") {
+                                        return a.ExpectedTime - b.ExpectedTime;
+                                    }
+                                    return 0;
+                                }).map((task, index) => (
+                                    <View key={index} style={styles.tasksTaskPreview}>
+                                        <View>
+                                            <Text style={styles.tasksTaskPreviewTitle}>{task.TaskName}</Text>
+                                            <Text style={styles.tasksDate}>{task.EndTime ? new Date(task.EndTime).toLocaleString("zh-TW", {
+                                                year: 'numeric',
+                                                month: '2-digit',
+                                                day: '2-digit',
+                                                hour: '2-digit',
+                                                minute: '2-digit',
+                                                hour12: true
+                                            }).replace(/\//g, "/") : "無"}</Text>
+                                        </View>
+                                        <Icon name="chevron-right" size={30} color="#aaa" style={{ alignSelf: "center", paddingLeft: 10 }} />
                                     </View>
-                                    <Icon name="chevron-right" size={30} color="#aaa" style={{ alignSelf: "center", paddingLeft: 10 }} />
-                                </View>
-                            ))
-                        ) : (
-                            <Text style={styles.tasksNoTasksText}>目前無待辦事項</Text>
-                        )
+                                ))
+                            ) : (
+                                <Text style={styles.tasksNoTasksText}>目前無{showCompleted ? "已完成" : "代辦"}事項</Text>
+                            );
+                        })()
                     }
                 </View>
             </ScrollView>
